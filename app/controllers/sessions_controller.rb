bd6a6432824bb269ef790be3df72fd2c870523bf
miShelbyT <mi.shelbyrose@gmail.com>
2020-10-16 16:04:16 -0400

    reverting some changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,15 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user = User.find_by(name: params[:user][:name])
-		user = user.try(:authenticate, params[:user][:password])
-		return redirect_to(controller: 'sessions', action: 'new') unless user
-		session[:user_id] = user.id
-		@user = user
-		redirect_to controller: 'welcome', action: 'home'
-	end
+    user = User.find_by(name: params[:user][:name])
+    user = user.try(:authenticate, params[:user][:password])
+    redirect_to(controller: 'sessions', action: 'new') unless user
+    session[:user_id] = user.id
+    @user = user
+    redirect_to controller: 'welcome', action: 'home'
+  end
 
 	def destroy
-		session.delete :user_id
-		redirect_to '/'
+		
 	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,6 @@
 class UsersController < ApplicationController
-	def new
-	end
 
 	def create
-    @user = User.create(user_params)
-    redirect_to controller: 'users', action: 'new' unless @user.save
-    session[:user_id] = @user.id
-    redirect_to controller: 'welcome', action: 'home'
-  end
+	end
 
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :password, :password_confirmation)
-  end
 end
