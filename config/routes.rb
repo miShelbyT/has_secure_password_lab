8711957cfda236d0d48c73b0fafda335a6e7a24b
miShelbyT <mi.shelbyrose@gmail.com>
2020-10-16 15:55:00 -0400

    work in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+	
+	get '/login', to: 'sessions#new'
+	post '/login', to: 'sessions#create'
+	post '/logout', to: 'sessions#destroy'
+
+	resources :users, only: [:new, :create]
+
+  root 'welcome#home'
+	# root '/login', to: 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
